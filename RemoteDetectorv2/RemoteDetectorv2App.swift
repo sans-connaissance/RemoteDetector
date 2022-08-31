6e3a02dcbb3a0b2a158892203a52ef334de74f13
David Malicke <davemalicke@gmail.com>
2022-08-31 12:29:47 -0400

    Initial Commit

---
+++ RemoteDetectorv2/RemoteDetectorv2App.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteDetectorv2App.swift
+//  RemoteDetectorv2
+//
+//  Created by David Malicke on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct RemoteDetectorv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
